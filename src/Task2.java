4bb8bdffd7521722928e3948bc1eae9d1697bb3a
sauravpatel186 <thespatel186@gmail.com>
2025-09-08 22:32:51 -0400

    Task_2_Complete

--- src/Task2.java
+++ src/Task2.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Task2 {
+    private float AOR(float length, float width){
+        return length * width;
+    }
+    public static void main(String[] args){
+        float l = 4.0f;
+        float w = 3.0f;
+        Task2 aor= new Task2();
+        System.out.print(aor.AOR(l,w));
+    }
 }
