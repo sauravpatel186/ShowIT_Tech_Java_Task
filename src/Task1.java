c5d5f1850cbfc77edbab1c753f512e1528935c18
sauravpatel186 <thespatel186@gmail.com>
2025-09-08 22:25:38 -0400

    Task_1_Complete

---
+++ src/Task1.java
@@ -0,0 +1,18 @@
+public class Task1 {
+    public static void main(String args[]){
+        String firstName = "Saurav";
+        String lastName = "Patel";
+        String address = "Thunder Bay";
+        int age = 25;
+        char bloodGroup = 'O';
+        System.out.println(firstName);
+
+        System.out.println(lastName);
+
+        System.out.println(address);
+
+        System.out.println(age);
+
+        System.out.println(bloodGroup);
+    }
+}
